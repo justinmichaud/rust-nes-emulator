e3d1a76a5d2e0c326c02a8a0c0737902cd7d19cf
Justin Michaud <justin@justinmichaud.com>
2017-06-03 15:27:17 -0400

    Add writable memory

--- src/mem.rs
+++ src/mem.rs
@@ -27,14 +27,14 @@ impl Mem {
             0x4000...0x4017 => /* apu */ 0,
             0x4020...0xFFFF => self.mapper_read(addr),
             _ => {
-                assert!(false, "Write to invalid address {:X}", addr);
+                assert!(false, "Read from invalid address {:X}", addr);
                 0xBF
             }
         }
     }
 
     pub fn read16(&self, addr: u16) -> u16 {
-        self.read(addr) as u16 + (self.read(addr+1) as u16)<<8
+        self.read(addr) as u16 + ((self.read(addr+1) as u16)<<8)
     }
 
     fn mirror_read(&self, from : RangeInclusive<u16>, to : RangeInclusive<u16>, addr : u16) -> u8 {
@@ -60,8 +60,55 @@ impl Mem {
                 }
             },
             _ => {
-                assert!(false, "Write to invalid address {:X}", addr);
+                assert!(false, "Read from invalid mapper address {:X}", addr);
                 0xBF
+            }
+        }
+    }
+
+    pub fn write(&mut self, addr: u16, val: u8) {
+        match addr as usize {
+            0...0x07FF => self.ram[addr as usize] = val,
+            0x0800...0x1FFF => self.mirror_write(0...0x07FF, 0x0800...0x1FFF, addr, val),
+            0x2000...0x2007 => /* ppu */ (),
+            0x2008...0x3FFF => self.mirror_write(0x2000...0x2007, 0x2008...0x3FFF, addr, val),
+            0x4000...0x4017 => /* apu */ (),
+            0x4020...0xFFFF => self.mapper_write(addr, val),
+            _ => {
+                assert!(false, "Write to invalid address {:X}", addr);
+            }
+        }
+    }
+
+    pub fn write16(&mut self, addr: u16, val: u16) {
+        self.write(addr, (val&0x00FF) as u8);
+        self.write(addr+1, ((val&0xFF00)>>8) as u8);
+    }
+
+    fn mirror_write(&mut self, from : RangeInclusive<u16>, to : RangeInclusive<u16>, addr : u16, val: u8) {
+        let size = from.end - from.start + 1;
+
+        let offset = (addr - to.start) % size;
+        self.write(from.start + offset, val)
+    }
+
+    fn mapper_write(&mut self, addr: u16, val: u8) {
+        assert!(self.prg_ram.len() == 8*1024, "PRG ram must not be mirrored (yet)");
+        assert!(self.prg.len() == 16*1024 || self.prg.len() == 32*1024, "PRG ram must be 16 or 32kb");
+
+        match addr as usize {
+            0x6000...0x7FFF => self.prg_ram[addr as usize - 0x6000] = val,
+            0x8000...0xBFFF => self.prg[addr as usize - 0x8000] = val,
+            0xC000...0xFFFF =>  {
+                if self.prg.len() == 32*1024 {
+                    self.prg[addr as usize - 0x8000] = val
+                }
+                    else {
+                        self.mirror_write(0x8000...0xBFFF, 0xC000...0xFFFF, addr, val)
+                    }
+            },
+            _ => {
+                assert!(false, "Write to invalid mapper address {:X}", addr);
             }
         }
     }
