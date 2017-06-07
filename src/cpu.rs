c857e4160124c92246ece39284eff4e903f3cecc
Justin Michaud <justin@justinmichaud.com>
2017-06-06 22:47:25 -0400

    improve addressing modes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,39 @@
 use mem::*;
 use phf::Map;
+use std::fmt;
 
-type AddressMode = fn(&mut Cpu, &Mem, bool) -> u16;
+enum AddressModeResult {
+    Val(u8),
+    Addr(u16)
+}
+use self::AddressModeResult::*;
+
+impl fmt::Debug for AddressModeResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Val(val) => write!(f, "Value ({:X})", val),
+            Addr(addr) => write!(f, "Address ({:X})", addr)
+        }
+    }
+}
+
+impl AddressModeResult {
+    fn read(&self, mem: &Mem) -> u8 {
+        match *self {
+            Val(val) => val,
+            Addr(addr) => mem.read(addr)
+        }
+    }
+
+    fn write(&self, mem: &mut Mem, val: u8) {
+        match *self {
+            Addr(addr) => mem.write(addr, val),
+            _ => panic!("Attempt to write to a read-only AddressModeResult: {:?}", self)
+        }
+    }
+}
+
+type AddressMode = fn(&mut Cpu, &Mem, bool) -> AddressModeResult;
 type ALUOperation = fn(&mut Cpu, &mut Mem, AddressMode) -> ();
 
 const OPCODES: Map<u8, (ALUOperation, AddressMode)> = phf_map!{
@@ -31,67 +63,68 @@ pub struct Cpu {
     count: u32
 }
 
-fn immediate(cpu: &mut Cpu, mem: &Mem, _: bool) -> u16 {
+fn immediate(cpu: &mut Cpu, mem: &Mem, _: bool) -> AddressModeResult {
     cpu.pc += 1;
     cpu.count += 2;
-    mem.read(cpu.pc-1) as u16
+    Val(mem.read(cpu.pc-1))
 }
 
-fn zero_page(cpu: &mut Cpu, mem: &Mem, _: bool) -> u16 {
+fn zero_page(cpu: &mut Cpu, mem: &Mem, _: bool) -> AddressModeResult {
+    let arg = mem.read(cpu.pc);
     cpu.pc += 1;
     cpu.count += 3;
-    mem.read((cpu.pc-1)%256) as u16
+    Addr(arg as u16)
 }
 
-fn zero_page_x(cpu: &mut Cpu, mem: &Mem, _: bool) -> u16 {
+fn zero_page_x(cpu: &mut Cpu, mem: &Mem, _: bool) -> AddressModeResult {
     let arg = mem.read(cpu.pc);
     cpu.pc += 1;
     cpu.count += 4;
-    (arg as u16 + cpu.x as u16) % 256
+    Addr((arg as u16 + cpu.x as u16) % 256)
 }
 
-fn zero_page_y(cpu: &mut Cpu, mem: &Mem, _: bool) -> u16 {
+fn zero_page_y(cpu: &mut Cpu, mem: &Mem, _: bool) -> AddressModeResult {
     let arg = mem.read(cpu.pc);
     cpu.pc += 1;
     cpu.count += 4;
-    (arg as u16 + cpu.y as u16) % 256
+    Addr((arg as u16 + cpu.y as u16) % 256)
 }
 
-fn absolute(cpu: &mut Cpu, mem: &Mem, _: bool) -> u16 {
+fn absolute(cpu: &mut Cpu, mem: &Mem, _: bool) -> AddressModeResult {
     cpu.pc += 2;
     cpu.count += 4;
-    mem.read16(cpu.pc-2)
+    Addr(mem.read16(cpu.pc-2))
 }
 
-fn absolute_x(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> u16 {
-    let arg = mem.read(cpu.pc);
+fn absolute_x(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> AddressModeResult {
+    let arg = mem.read16(cpu.pc);
     cpu.pc += 1;
     cpu.count += 4;
     if page_matters && (arg as u16 + cpu.x as u16)/256u16 != arg as u16 / 256u16 {
         cpu.count += 1;
     }
-    arg as u16 + cpu.x as u16
+    Addr(arg + cpu.x as u16)
 }
 
-fn absolute_y(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> u16 {
-    let arg = mem.read(cpu.pc);
+fn absolute_y(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> AddressModeResult {
+    let arg = mem.read16(cpu.pc);
     cpu.pc += 1;
     cpu.count += 4;
     if page_matters && (arg as u16 + cpu.y as u16)/256u16 != arg as u16/256u16 {
         cpu.count += 1;
     }
-    arg as u16 + cpu.y as u16
+    Addr(arg + cpu.y as u16)
 }
 
-fn indirect_x(cpu: &mut Cpu, mem: &Mem, _: bool) -> u16 {
+fn indirect_x(cpu: &mut Cpu, mem: &Mem, _: bool) -> AddressModeResult {
     let arg = mem.read(cpu.pc);
     cpu.pc += 1;
     cpu.count += 6;
-    mem.read((arg as u16 + cpu.x as u16) % 256) as u16
-        + (mem.read((arg as u16 + cpu.x as u16 + 1) % 256) as u16)*256
+    Addr(mem.read((arg as u16 + cpu.x as u16) % 256) as u16
+        + (mem.read((arg as u16 + cpu.x as u16 + 1) % 256) as u16)*256)
 }
 
-fn indirect_y(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> u16 {
+fn indirect_y(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> AddressModeResult {
     let arg = mem.read(cpu.pc);
     cpu.pc += 1;
     cpu.count += 5;
@@ -102,12 +135,12 @@ fn indirect_y(cpu: &mut Cpu, mem: &Mem, page_matters: bool) -> u16 {
         cpu.count += 1;
     }
 
-    base + cpu.y as u16
+    Addr(base + cpu.y as u16)
 }
 
 fn adc(cpu: &mut Cpu, mem: &mut Mem, mode: AddressMode) {
     let addr = mode(cpu, mem, true);
-    println!("Val {:X}, {:X}", addr);
+    println!("{:?} => {:X}", addr, addr.read(mem));
 }
 
 fn manual(cpu: &mut Cpu, mem: &mut Mem, op: u8) {
--- src/mem.rs
+++ src/mem.rs
@@ -8,6 +8,13 @@ pub struct Mem {
     ram: [u8; 2 * 1024]
 }
 
+enum MemoryRef {
+    Ram(usize),
+    Prg(usize),
+    PrgRam(usize)
+}
+use self::MemoryRef::*;
+
 impl Mem {
     pub fn new(prg: Vec<u8>, chr: Vec<u8>, prg_ram_size: usize) -> Mem {
         Mem {
@@ -19,17 +26,10 @@ impl Mem {
     }
 
     pub fn read(&self, addr: u16) -> u8 {
-        match addr as usize {
-            0...0x07FF => self.ram[addr as usize],
-            0x0800...0x1FFF => self.mirror_read(0...0x07FF, 0x0800...0x1FFF, addr),
-            0x2000...0x2007 => /* ppu */ 0,
-            0x2008...0x3FFF => self.mirror_read(0x2000...0x2007, 0x2008...0x3FFF, addr),
-            0x4000...0x4017 => /* apu */ 0,
-            0x4020...0xFFFF => self.mapper_read(addr),
-            _ => {
-                assert!(false, "Read from invalid address {:X}", addr);
-                0xBF
-            }
+        match self.mem_ref(addr) {
+            Ram(addr) => self.ram[addr],
+            Prg(addr) => self.prg[addr],
+            PrgRam(addr) => self.prg_ram[addr]
         }
     }
 
@@ -37,46 +37,11 @@ impl Mem {
         self.read(addr) as u16 + ((self.read(addr+1) as u16)<<8)
     }
 
-    fn mirror_read(&self, from : RangeInclusive<u16>, to : RangeInclusive<u16>, addr : u16) -> u8 {
-        let size = from.end - from.start + 1;
-
-        let offset = (addr - to.start) % size;
-        self.read(from.start + offset)
-    }
-
-    fn mapper_read(&self, addr: u16) -> u8 {
-        assert!(self.prg_ram.len() == 8*1024, "PRG ram must not be mirrored (yet)");
-        assert!(self.prg.len() == 16*1024 || self.prg.len() == 32*1024, "PRG ram must be 16 or 32kb");
-
-        match addr as usize {
-            0x6000...0x7FFF => self.prg_ram[addr as usize - 0x6000],
-            0x8000...0xBFFF => self.prg[addr as usize - 0x8000],
-            0xC000...0xFFFF =>  {
-                if self.prg.len() == 32*1024 {
-                    self.prg[addr as usize - 0x8000]
-                }
-                else {
-                    self.mirror_read(0x8000...0xBFFF, 0xC000...0xFFFF, addr)
-                }
-            },
-            _ => {
-                assert!(false, "Read from invalid mapper address {:X}", addr);
-                0xBF
-            }
-        }
-    }
-
     pub fn write(&mut self, addr: u16, val: u8) {
-        match addr as usize {
-            0...0x07FF => self.ram[addr as usize] = val,
-            0x0800...0x1FFF => self.mirror_write(0...0x07FF, 0x0800...0x1FFF, addr, val),
-            0x2000...0x2007 => /* ppu */ (),
-            0x2008...0x3FFF => self.mirror_write(0x2000...0x2007, 0x2008...0x3FFF, addr, val),
-            0x4000...0x4017 => /* apu */ (),
-            0x4020...0xFFFF => self.mapper_write(addr, val),
-            _ => {
-                assert!(false, "Write to invalid address {:X}", addr);
-            }
+        match self.mem_ref(addr) {
+            Ram(addr) => self.ram[addr] = val,
+            Prg(addr) => self.prg[addr] = val,
+            PrgRam(addr) => self.prg_ram[addr] = val
         }
     }
 
@@ -85,30 +50,44 @@ impl Mem {
         self.write(addr+1, ((val&0xFF00)>>8) as u8);
     }
 
-    fn mirror_write(&mut self, from : RangeInclusive<u16>, to : RangeInclusive<u16>, addr : u16, val: u8) {
+    fn mem_ref(&self, addr: u16) -> MemoryRef {
+        match addr as usize {
+            0...0x07FF => Ram(addr as usize),
+            0x0800...0x1FFF => self.mirror_ref(0...0x07FF, 0x0800...0x1FFF, addr),
+            //0x2000...0x2007 => /* ppu */,
+            0x2008...0x3FFF => self.mirror_ref(0x2000...0x2007, 0x2008...0x3FFF, addr),
+            //0x4000...0x4017 => /* apu */,
+            0x4020...0xFFFF => self.mapper_ref(addr),
+            _ => {
+                panic!("Reference to invalid address {:X}", addr);
+            }
+        }
+    }
+
+    fn mirror_ref(&self, from : RangeInclusive<u16>, to : RangeInclusive<u16>, addr : u16) -> MemoryRef {
         let size = from.end - from.start + 1;
 
         let offset = (addr - to.start) % size;
-        self.write(from.start + offset, val)
+        self.mem_ref(from.start + offset)
     }
 
-    fn mapper_write(&mut self, addr: u16, val: u8) {
+    fn mapper_ref(&self, addr: u16) -> MemoryRef {
         assert!(self.prg_ram.len() == 8*1024, "PRG ram must not be mirrored (yet)");
         assert!(self.prg.len() == 16*1024 || self.prg.len() == 32*1024, "PRG ram must be 16 or 32kb");
 
         match addr as usize {
-            0x6000...0x7FFF => self.prg_ram[addr as usize - 0x6000] = val,
-            0x8000...0xBFFF => self.prg[addr as usize - 0x8000] = val,
+            0x6000...0x7FFF => PrgRam(addr as usize - 0x6000),
+            0x8000...0xBFFF => Prg(addr as usize - 0x8000),
             0xC000...0xFFFF =>  {
                 if self.prg.len() == 32*1024 {
-                    self.prg[addr as usize - 0x8000] = val
+                    Prg(addr as usize - 0x8000)
                 }
-                    else {
-                        self.mirror_write(0x8000...0xBFFF, 0xC000...0xFFFF, addr, val)
-                    }
+                else {
+                    self.mirror_ref(0x8000...0xBFFF, 0xC000...0xFFFF, addr)
+                }
             },
             _ => {
-                assert!(false, "Write to invalid mapper address {:X}", addr);
+                panic!("Reference to invalid mapper address {:X}", addr);
             }
         }
     }
