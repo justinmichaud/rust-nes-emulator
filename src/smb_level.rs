0e5353e1011395a8af6c30a67fadd85c6f10bdc9
Justin Michaud <justin@justinmichaud.com>
2017-07-06 22:22:03 -0400

    start adding level loader

--- src/smb_level.rs
+++ src/smb_level.rs
@@ -1,4 +1,5 @@
 use nes::*;
+use ines::lines_from_file;
 
 pub struct SmbLevel {
 
@@ -12,54 +13,42 @@ impl SmbLevel {
     pub fn load(&mut self, chipset: &mut Chipset) {
         chipset.write(0x8000 - 16 + 0x1CCC, 0x25); // Set area
 
-        // Level data
-        {
-            const num_objects: usize = 16*8*3;
-            const len: usize = num_objects*2 + 3;
-            let mut my_level: [u8; len] = [0; len];
-            my_level[0] = 0b10_100_000;
-            my_level[1] = 0b10_00_0000;
-            my_level[len - 1] = 0xFD;
+        let mut level_objects = vec![0b10_100_000, 0b10_00_0000];
+        let mut enemy_objects = vec![];
+        let level_in = lines_from_file("assets/0.level");
 
-            let mut index = 0;
-            for p_x in 0..8 {
-                for x in 0..16 {
-                    for y in 0..3 {
-                        my_level[index*2+2] = ((x&0x0F)<<4) + ((0b1011-y)&0x0F);
-                        my_level[index*2+3] = if x == 0 && y==0 && p_x>0 { 0b10000000 } else { 0 }
-                            + if x == 0 { 0 } else { 4 };
+        let mut last_page_level = 0;
+        let mut last_page_enemy = 0;
 
-                        index += 1;
-                    }
+        for x in 0..level_in[0].len() {
+            for y in (0...level_in.len()-1).rev() {
+                let c = level_in.get(y).unwrap().chars().nth(x).unwrap();
+
+                let (mut number, arr, page) = match c {
+                    '.' => (0x04, &mut level_objects, &mut last_page_level),
+                    'g' => (0x06, &mut enemy_objects, &mut last_page_enemy),
+                    _ => continue
+                };
+
+                if x/16 > *page {
+                    number |= 0b10000000;
+                    *page = x/16;
                 }
-            }
 
-            assert!(index == num_objects);
-
-            for i in 0..len {
-                chipset.write(0x8000 - 16 + 0x269E + i as u16, my_level[i]);
+                arr.push((((x&0x0F) as u8) << 4) + ((y as u8) & 0x0F));
+                arr.push(number);
             }
         }
 
-        // Sprite data
-        {
-            let mut my_level = [
-                0x0b, 0x86,
-                0x1b, 0x06,
-                0x2b, 0x06,
-                0x3b, 0x06,
-                0x4b, 0x06,
-                0x6a, 0x86,
-                0x7a, 0x06,
-                0x8a, 0x06,
-                0x9a, 0x06,
-                0xaa, 0x06,
-                0xFF,
-            ];
+        level_objects.push(0xFD);
+        enemy_objects.push(0xFF);
 
-            for i in 0..my_level.len() {
-                chipset.write(0x8000 - 16 + 0x1F11 + i as u16, my_level[i]);
-            }
+        for i in 0..level_objects.len() {
+            chipset.write(0x8000 - 16 + 0x269E + i as u16, level_objects[i]);
+        }
+
+        for i in 0..enemy_objects.len() {
+            chipset.write(0x8000 - 16 + 0x1F11 + i as u16, enemy_objects[i]);
         }
     }
 }
